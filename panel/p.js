a5ee7c9d83043678c98a2ef6f01719c933eb0595
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-06-03 14:04:50 +0530

    Update p.js

--- panel/p.js
+++ panel/p.js
@@ -1,32 +1,27 @@
 const openPopupBtn = document.getElementById('openPopupBtn');
-    const overlay = document.getElementById('overlay');
-    const popup = document.getElementById('popup');
-    const submitPasswordBtn = document.getElementById('submitPasswordBtn');
-    const passwordInput = document.getElementById('passwordInput');
-    const popupContent = document.getElementById('popupContent');
-    const popupHeader = document.getElementById('popupHeader');
-    const popupBody = document.getElementById('popupBody');
+const overlay = document.getElementById('overlay');
+const popup = document.getElementById('popup');
+const submitPasswordBtn = document.getElementById('submitPasswordBtn');
+const passwordInput = document.getElementById('passwordInput');
+const popupContent = document.getElementById('popupContent');
+const popupHeader = document.getElementById('popupHeader');
+const popupBody = document.getElementById('popupBody');
 
-    const correctPassword = 'vivekmasona'; // Set the desired password
+openPopupBtn.addEventListener('click', () => {
+    overlay.style.display = 'block';
+    popup.style.display = 'block';
+});
 
-    openPopupBtn.addEventListener('click', () => {
-        overlay.style.display = 'block';
-        popup.style.display = 'block';
-    });
+overlay.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    popup.style.display = 'none';
+    popupContent.style.display = 'none';
+});
 
-    overlay.addEventListener('click', () => {
-        overlay.style.display = 'none';
-        popup.style.display = 'none';
-        popupContent.style.display = 'none';
-    });
+submitPasswordBtn.addEventListener('click', () => {
+    popupContent.style.display = 'block';
+    popupHeader.style.display = 'none';
+    popupBody.style.display = 'none';
+    popupContent.style.height = '100%'; // Ensure the iframe takes full height
+});
 
-    submitPasswordBtn.addEventListener('click', () => {
-        if (passwordInput.value === correctPassword) {
-            popupContent.style.display = 'block';
-            popupHeader.style.display = 'none';
-            popupBody.style.display = 'none';
-            popupContent.style.height = '100%'; // Ensure the iframe takes full height
-        } else {
-            alert('Incorrect password!');
-        }
-    });
