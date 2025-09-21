1349b1cde752e74f6974eb6f8359ad1e22f57d0c
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2025-09-21 07:55:02 +0530

    Update suggestion.js

--- autocomplate/suggestion.js
+++ autocomplate/suggestion.js
@@ -1,41 +1,59 @@
-$(document).ready(function () {
-  var searchField = $("input.search-bar");
-  var autocompleteResults = $("#autocomplete-results");
-  var typingTimer;
+$(function () {
+  var searchField = $(".search-bar");
+  var ghost = $(".autocomplete-ghost");
+  var suggestionList = [];
+  var activeSuggestion = "";
 
-  searchField.on("input", function () {
-    var term = $(this).val();
-    if (term.length < 1) {
-      autocompleteResults.empty();
+  // Google suggestion fetch
+  function fetchSuggestions(query) {
+    if (!query) {
+      ghost.text("");
       return;
     }
-
     $.ajax({
       url: "//clients1.google.com/complete/search",
       dataType: "jsonp",
-      data: {
-        q: term,
-        nolabels: "t",
-        client: "youtube",
-        ds: "yt"
-      },
+      data: { q: query, nolabels: "t", client: "youtube", ds: "yt" },
       success: function (r) {
-        var data = r[1].map(function (item) {
-          return item[0];
-        });
-
-        autocompleteResults.empty();
-        $.each(data, function (index, value) {
-          autocompleteResults.append("<p class='autocomplete-suggestion'>" + value + "</p>");
-        });
+        suggestionList = r[1].map(item => item[0]);
+        showGhost(query);
       }
     });
+  }
+
+  // Show faded suggestion
+  function showGhost(query) {
+    activeSuggestion = suggestionList.find(s =>
+      s.toLowerCase().startsWith(query.toLowerCase())
+    ) || "";
+    if (activeSuggestion) {
+      ghost.text(activeSuggestion);
+    } else {
+      ghost.text("");
+    }
+  }
+
+  // On typing
+  searchField.on("input", function () {
+    var val = $(this).val();
+    ghost.text("");
+    fetchSuggestions(val);
   });
 
-  autocompleteResults.on("click", ".autocomplete-suggestion", function () {
-    var suggestion = $(this).text();
-    searchField.val(suggestion);
-    autocompleteResults.empty();
-    $("#search-btn").click();
+  // Right arrow or Tab = accept suggestion
+  searchField.on("keydown", function (e) {
+    if ((e.key === "ArrowRight" || e.key === "Tab") && activeSuggestion) {
+      e.preventDefault();
+      $(this).val(activeSuggestion);
+      ghost.text("");
+    }
+  });
+
+  // Click inside input = accept suggestion
+  searchField.on("click", function () {
+    if (activeSuggestion) {
+      $(this).val(activeSuggestion);
+      ghost.text("");
+    }
   });
 });
