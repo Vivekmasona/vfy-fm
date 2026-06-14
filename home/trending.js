0028824ce7eee6e2b368f13a1dc3f8cf78f5c1a9
Vivek maurya <112853103+Vivekmasona@users.noreply.github.com>
2026-06-14 17:13:00 +0530

    Update trending.js

--- home/trending.js
+++ home/trending.js
@@ -16,8 +16,8 @@ async function fetchTrendingSongs(query) {
         return [];
     }
 
-    const denoApiUrl = `https://vivekmasona-denocall-61.deno.dev/search?q=${encodeURIComponent(query)}`;
-    const selfApiUrl = `https://self-lac.vercel.app/v3-api?q=${encodeURIComponent(query)}`;
+    const denoApiUrl = `https://vivekmasona-denocall-61.deno.dev/search?qq=${encodeURIComponent(query)}`;
+    const selfApiUrl = `https://self-lac.vercel.app/v3-api?qq=${encodeURIComponent(query)}`;
 
     try {
 
